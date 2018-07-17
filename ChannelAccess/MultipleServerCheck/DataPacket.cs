319b6fd6b6b5da4501def6b58da778dc7873d9d4
Bertrand Alain Gregor <alain.bertrand@psi.ch>
2018-07-17 11:52:55 +0000

    Added tool to check multiple servers answers
    git-tfs-id: [https://tfs.psi.ch/DefaultCollection]$/EpicsLibrary;C2707

---
+++ ChannelAccess/MultipleServerCheck/DataPacket.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleServerCheck
+{
+    /// <summary>
+    /// Simplified and minimalistic Data Packet.
+    /// Warning: This doesn't cover all the cases, don't use it as in production.
+    /// </summary>
+    public class DataPacket
+    {
+        public byte[] Data;
+
+        public bool ExtendedMessage => (GetUInt16(2) == 0xFFFF && GetUInt16(6) == 0x0000);
+
+        public UInt16 Command
+        {
+            get
+            {
+                return GetUInt16(0);
+            }
+            set
+            {
+                SetUInt16(0, value);
+            }
+        }
+
+        public UInt32 PayloadSize => ExtendedMessage ? GetUInt32(16) : GetUInt16(2);
+
+        public UInt16 DataType
+        {
+            get
+            {
+                return GetUInt16(4);
+            }
+            set
+            {
+                SetUInt16(4, value);
+            }
+        }
+
+        public UInt32 DataCount
+        {
+            get
+            {
+                if (ExtendedMessage)
+                    return GetUInt32(20);
+                return GetUInt16(6);
+            }
+            set
+            {
+                if (ExtendedMessage)
+                    SetUInt32(20, value);
+                else
+                    SetUInt16(6, (UInt16)value);
+            }
+        }
+
+        public UInt32 Parameter1
+        {
+            get
+            {
+                return GetUInt32(8);
+            }
+            set
+            {
+                SetUInt32(8, value);
+            }
+        }
+
+        public UInt32 Parameter2
+        {
+            get
+            {
+                return GetUInt32(12);
+            }
+            set
+            {
+                SetUInt32(12, value);
+            }
+        }
+
+        public UInt32 MessageSize => PayloadSize + HeaderSize;
+
+        public UInt32 HeaderSize => (UInt32)(ExtendedMessage ? 24 : 16);
+
+        public void SetDataAsString(string str)
+        {
+            byte[] b = Encoding.Default.GetBytes(str);
+            Array.Clear(Data, 16, Data.Length - 16);
+            Buffer.BlockCopy(b, 0, Data, 16, b.Length);
+        }
+
+        public string GetDataAsString(int offset = 0, int maxSize = 40)
+        {
+            // If data is smaller than what it should... return empty string
+            if ((ExtendedMessage ? 24 : 16) + (int)PayloadSize > Data.Length)
+                return "";
+            offset += (int)this.HeaderSize;
+            string ret = Encoding.Default.GetString(Data, offset, Math.Min(Data.Length - offset, maxSize));
+            int indexOf = ret.IndexOf('\0');
+            if (indexOf != -1)
+                ret = ret.Substring(0, indexOf);
+            return ret;
+        }
+
+        public UInt16 GetUInt16(int position)
+        {
+            return (UInt16)(((uint)Data[position] << 8) | Data[position + 1]);
+        }
+
+        public UInt32 GetUInt32(int position)
+        {
+            return (UInt32)(((uint)Data[position + 0] << 24) | ((uint)Data[position + 1] << 16) | ((uint)Data[position + 2] << 8) | ((uint)Data[position + 3]));
+        }
+
+        public void SetUInt16(int position, UInt16 value)
+        {
+            Data[position] = (byte)((value & 0xFF00u) >> 8);
+            Data[position + 1] = (byte)((value) & 0xFFu);
+        }
+
+        public void SetUInt32(int position, UInt32 value)
+        {
+            Data[position + 0] = (byte)((value & 0xFF000000u) >> 24);
+            Data[position + 1] = (byte)((value & 0x00FF0000u) >> 16);
+            Data[position + 2] = (byte)((value & 0x0000FF00u) >> 8);
+            Data[position + 3] = (byte)(value & 0x000000FFu);
+        }
+
+        public static DataPacket Create(int size)
+        {
+            DataPacket p = new DataPacket();
+            p.Data = new byte[size + 16];
+            p.SetUInt16(2, (ushort)(size));
+            return p;
+        }
+
+        internal static DataPacket Create(byte[] buffer, int offset, int nb)
+        {
+            DataPacket p = new DataPacket();
+            p.Data = new byte[nb];
+            Array.Copy(buffer, offset, p.Data, 0, nb);
+            return p;
+        }
+
+        private DataPacket()
+        {
+        }
+    }
+
+}
---
+++ ChannelAccess/MultipleServerCheck/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleServerCheck
+{
+    class Program
+    {
+        const int CATimeout = 5000;
+
+        static void Main(string[] args)
+        {
+            string channelName = "";
+            string config = "255.255.255.255:5064";
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("MultipleServerCheck [-e <searchConfig>] <channelName>");
+                return;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (args[i] == "-e")
+                {
+                    i++;
+                    config = args[i];
+                }
+                else
+                    channelName = args[i];
+            }
+
+            CaGet(channelName, config).Wait();
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        static async Task CaGet(string channelName, string epicsConfig)
+        {
+            IPEndPoint remoteEP;
+
+            // We first send a search packet
+            // Using UdpClient as wraper class for our UDP communication
+            using (var udpClient = new UdpClient(5432)) // listening port
+            {
+                var search = DataPacket.Create(channelName.Length + 8 - channelName.Length % 8);
+                search.Command = 6; // CA_PROTO_SEARCH
+                search.DataType = 4; // DONT_REPLY
+                search.DataCount = 11; // MINOR PROTO VERSION
+                search.Parameter1 = 1; // CID
+                search.Parameter2 = 1; // CID
+                search.SetDataAsString(channelName);
+
+                foreach (var c in ParseAddress(epicsConfig))
+                {
+                    Console.WriteLine("Searching in " + c);
+                    udpClient.Connect(c);
+                    udpClient.Send(search.Data, search.Data.Length);
+
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    // No response before timeout, means no channel
+
+                    List<string> knownServers = new List<string>();
+                    bool mustStop = false;
+                    while (!mustStop)
+                    {
+                        var receivedDataTask = udpClient.ReceiveAsync();
+                        if (await Task.WhenAny(receivedDataTask, Task.Delay(CATimeout)) != receivedDataTask)
+                        {
+                            mustStop = true;
+                            break;
+                        }
+                        remoteEP = receivedDataTask.Result.RemoteEndPoint;
+                        if (!knownServers.Contains(remoteEP.ToString()))
+                        {
+                            Console.WriteLine("Response from " + remoteEP.ToString() + " after " + sw.Elapsed.ToString());
+                            knownServers.Add(remoteEP.ToString());
+                        }
+                    }
+                }
+            }
+        }
+
+        static public IEnumerable<IPEndPoint> ParseAddress(string addrs)
+        {
+            foreach (var a in addrs.Replace(",", ";").Split(';'))
+            {
+                string[] parts = a.Split(':');
+                IPEndPoint res;
+                try
+                {
+                    res = new IPEndPoint(IPAddress.Parse(parts[0].Trim()), int.Parse(parts[1].Trim()));
+                }
+                //catch (Exception ex)
+                catch
+                {
+                    res = new IPEndPoint(Dns.GetHostEntry(parts[0]).AddressList.First(), int.Parse(parts[1].Trim()));
+                }
+                yield return res;
+            }
+        }
+    }
+}
